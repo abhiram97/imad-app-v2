695d7cc99e315567ad6837b4a86b7c576a3336b6
abhiram97 <abhiram1296@gmail.com>
2017-02-05 17:22:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName',function(req,res){
     var articleName=req.params.articleName;
-   res.send(template(articles[articleName]);
+   res.send(template(articles[articleName]));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
