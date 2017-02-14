63ac1a3ac41ce75afd20ca8b43f652aa63a94dd7
abhiram97 <abhiram1296@gmail.com>
2017-02-14 20:03:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,6 @@ return htmltemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
 app.get('/:articleName',function(req,res){
     var articleName=req.params.articleName;
    res.send(template(article[articleName]));
