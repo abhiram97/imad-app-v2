4297baf282d3b5e48257729193c1175d3d869122
abhiram97 <abhiram1296@gmail.com>
2017-02-14 20:48:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,9 @@
 console.log('Loaded!');
 var sss= document.getElementById("boss");
+var marginLeft=0;
 function moveRight(){
     marginLeft= marginLeft + 10;
-    sss.style.marginRight= '200px';
+    sss.style.marginLeftt= marginLeft + 'px';
 }
 sss.onclick= function(){
   var interval= setInterval(moveRight, 100);
