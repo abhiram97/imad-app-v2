d65b38a31b02e4d4bd40b7a337f91fe710670948
abhiram97 <abhiram1296@gmail.com>
2017-02-14 21:57:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,18 @@
 var button= document.getElementById("but");
 var counter= 0;
 button.onclick= function(){
-  counter= counter +1;
-  span= document.getElementById("count");
-  span.innerHTML=counter.toString();
+    var request= new XMLHttprequest();
+    
+    request.onreadystatechange = function(){
+        if(request.readystate=== XMLHttprequest.DONE)
+        {
+            if(request.status=== 200)
+            {
+                span= document.getElementById("count");
+                span.innerHTML=counter.toString();               
+            }
+        }
+    };
+    request.open('GET','http://abhiram97.imad.hasura-app.io',true);
+    request.send(null);
 };
