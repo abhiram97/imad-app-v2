c417b9beebd61f16a16a15540c64c06354df2526
abhiram97 <abhiram1296@gmail.com>
2017-02-14 22:09:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.get('/ui/main.js', function (req, res) {
 var counter= 0;
 app.get('/counter', function(req,res){
    counter= counter + 1;
-   res.send("u r the "+counter.toString()+"th visitor");
+   res.send(counter.toString());
 });
 
 app.get('/ui/style.css', function (req, res) {
