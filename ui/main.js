2ef5387a7802f00c37cefad340d4146e4b9efc17
abhiram97 <abhiram1296@gmail.com>
2017-02-14 21:36:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,6 @@
-console.log('Loaded!');
-var sss= document.getElementById("boss");
-var marginLeft=0;
-function moveRight(){
-    marginLeft= marginLeft + 10;
-    sss.style.marginLeft= marginLeft + 'px';
-}
-sss.onclick= function(){
-  var interval= setInterval(moveRight, 50);
-};
-var a= document.getElementById("but");
-
-a.onclick= function(){
-  var b= document.getElementById("count");  
-  counter= counter + 1;
-  b.innerHTML=counter;
+var button= document.getElementById('but');
+button.onclick= function(){
+  counter= counter +1;
+  span= document.getElementById('count');
+  span.innerHTML=counter;
 };
